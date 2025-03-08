d353bd2acd6e00baeefd04257eba205ad43577ee
suchitd11 <dsuchit111@gmail.com>
2025-03-08 23:15:49 +0530

    Adding project setup code

--- src/test/java/com/suchit/dev/urlshortner/UrlshortnerApplicationTests.java
+++ src/test/java/com/suchit/dev/urlshortner/UrlshortnerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.suchit.dev.urlshortner;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UrlshortnerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.suchit.dev.urlshortner;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UrlshortnerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
