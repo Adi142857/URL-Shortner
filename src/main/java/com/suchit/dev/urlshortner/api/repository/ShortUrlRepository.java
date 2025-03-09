f2822aff9b8e363a9424253c94934a38ca8c454b
suchitd11 <dsuchit111@gmail.com>
2025-03-09 16:19:29 +0530

    Adding missing annotations

--- src/main/java/com/suchit/dev/urlshortner/api/repository/ShortUrlRepository.java
+++ src/main/java/com/suchit/dev/urlshortner/api/repository/ShortUrlRepository.java
@@ -2,9 +2,11 @@ package com.suchit.dev.urlshortner.api.repository;
 
 import com.suchit.dev.urlshortner.api.entity.ShortUrlEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ShortUrlRepository extends JpaRepository<ShortUrlEntity, Long> {
     ShortUrlEntity findByShortCode(String shortCode);
     ShortUrlEntity findByOriginalUrl(String originalUrl);
